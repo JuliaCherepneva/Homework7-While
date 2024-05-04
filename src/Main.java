176a3468c198365164745e93af349ca624c34290
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-05-04 14:19:09 +0400

    Выполненое домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,12 @@ public class Main {
         int total = 0;
         int month = 1;
         while (total < 2_459_000) {
-            total = total + (total/100);
             total = total + cash;
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
             month++;
         }
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+
+
         System.out.println("\nЗадание 2");
         int i = 1;
         while (i <= 10) {
@@ -24,6 +25,7 @@ public class Main {
         for (int g = 10; g >= 1; g--) {
             System.out.print(g + " ");
         }
+        System.out.println();
 
         System.out.println("\nЗадание 3");
         int population = 12_000_000;
@@ -37,5 +39,57 @@ public class Main {
             System.out.println("Год " + year + " , численность населения составляет " + population);
         }
 
+        System.out.println("\nЗадание 4");
+        {
+            int deposit = 15_000;
+            int depositPercent = 7;
+            int Month = 0;
+            while (deposit < 12_000_000) {
+                deposit = deposit + (deposit * depositPercent / 100);
+                Month++;
+                System.out.println("Месяц " + Month + ", сумма накоплений равна " + deposit+ " рублей");
+            }
+        }
+        System.out.println("\nЗадание 5");
+        {
+            int deposit = 15_000;
+            int depositPercent = 7;
+            int Month = 0;
+            while (deposit < 12_000_000) {
+                deposit = deposit + (deposit * depositPercent / 100);
+                Month++;
+                if (Month % 6 == 0) {
+                    System.out.println("Месяц " + Month + ", сумма накоплений равна " + deposit + " рублей");
+                }
+            }
+        }
+        System.out.println("\nЗадание 6");
+        {
+            int deposit = 15_000;
+            int depositPercent = 7;
+            int Month = 0;
+            while (Month <= 108) {
+                deposit = deposit + (deposit * depositPercent / 100);
+                Month++;
+                if (Month % 6 == 0) {
+                    System.out.println("Месяц " + Month + ", сумма накоплений равна " + deposit + " рублей");
+                }
+            }
+        }
+        System.out.println("\nЗадание 7");
+        for (int dayNumber = 7; dayNumber <= 31; dayNumber++) {
+            if (dayNumber % 7 == 0) {
+                System.out.println("Сегодня пятница " + dayNumber + " число. Необходимо подготовить отчет");
+            }
+        }
+        System.out.println("\nЗадание 8");
+        int now = 2024;
+        int befor = now - 200;
+        int after = now + 100;
+        for (int comet = befor; comet < after; comet++) {
+            if (comet % 79 == 0) {
+                System.out.println(comet);
+            }
+        }
     }
 }
